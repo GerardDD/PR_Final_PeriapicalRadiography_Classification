27bcef3843248019fb452258c159eca73191722c
Gerard Domenech <gerarddomenechdomingo@gmail.com>
2022-03-01 23:34:57 +0100

    modified the script so now it takes input folder and output folder as terminal parameters

--- Image preprocessing/image_preprocessing.py
+++ Image preprocessing/image_preprocessing.py
@@ -2,18 +2,19 @@ import numpy as np
 try:
     import cv2
 except:
-    print(' to run this script you will need to install OpenCV library')
+    print('to run this script you will need to install OpenCV library')
+    print('ending execution')
 import os
 
-# path = input('enter the path where your images are')
-path = '/Users/cnieto/Downloads/fets'
+path = input('enter the path where your images are')
+print('/n')
+outpath = input('enter the path where you want to save the censored images')
+# path = '/Users/cnieto/Downloads/fets'
 xrays = os.listdir(path)
 print(xrays)
 
 for i in xrays:
     image = cv2.imread(f'{path}/{i}')
-    cv2.imshow('original',image)
-    print("i'm in image")
     # extract 1/10 of the height
     ten = int(image.shape[0] /10)
     # extract 1/3 of the width
@@ -22,8 +23,8 @@ for i in xrays:
     cv2.rectangle(image, (0,0),(three,ten),(0,0,0),-1)
     #create black rectangle on top right
     cv2.rectangle(image, (image.shape[1],0),(image.shape[1] - three,ten),(0,0,0),-1)
-    print(f'/Users/cnieto/IronHack/Personal_projects/PR_Final_PeriapicalRadiography_Classification/Image preprocessing/testing/{i[:-4]}_cens.jpg')
-    cv2.imwrite(f'/Users/cnieto/IronHack/Personal_projects/PR_Final_PeriapicalRadiography_Classification/Image preprocessing/testing/{i[:-4]}_cens.jpg', image)
+    #print(f'/Users/cnieto/IronHack/Personal_projects/PR_Final_PeriapicalRadiography_Classification/Image preprocessing/testing/{i[:-4]}_cens.jpg')
+    cv2.imwrite(f'{outpath}/{i[:-4]}_cens.jpg', image)
 
 
 
