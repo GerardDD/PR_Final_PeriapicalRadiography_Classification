9764898011ad56445cbf1da4b4dc4586c4e26166
Gerard Domenech <gerarddomenechdomingo@gmail.com>
2022-03-10 21:07:15 +0100

    processed new batch of images with censor script

--- Image_preprocessing/image_preprocessing.py
+++ Image_preprocessing/image_preprocessing.py
@@ -15,13 +15,16 @@ xrays = os.listdir(path)
 print(xrays)
 
 for i in xrays:
-    image = cv2.imread(f'{path}/{i}')
-    # extract 1/10 of the height
-    ten = int(image.shape[0] /10)
-    # extract 1/3 of the width
-    three = int(image.shape[1] / 3)
-    #create black rectangle on top left
-    cv2.rectangle(image, (0,0),(three,ten),(0,0,0),-1)
-    #create black rectangle on top right
-    cv2.rectangle(image, (image.shape[1],0),(image.shape[1] - three,ten),(0,0,0),-1)
-    cv2.imwrite(f'{outpath}/{i[:-4]}_cens.jpg', image)
+    try:
+        image = cv2.imread(f'{path}/{i}')
+        # extract 1/10 of the height
+        ten = int(image.shape[0] /10)
+        # extract 1/3 of the width
+        three = int(image.shape[1] / 3)
+        #create black rectangle on top left
+        cv2.rectangle(image, (0,0),(three,ten),(0,0,0),-1)
+        #create black rectangle on top right
+        cv2.rectangle(image, (image.shape[1],0),(image.shape[1] - three,ten),(0,0,0),-1)
+        cv2.imwrite(f'{outpath}/{i[:-4]}_cens.jpg', image)
+    except:
+        pass
